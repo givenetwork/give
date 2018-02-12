558e60b7d80349232bdf158b6f506cab502dd660
Klem Novak (knovak@execproinc.com) <knovak@execproinc.com>
2018-02-12 09:07:10 -0800

    Clean navbar

--- app/Nav.vue
+++ app/Nav.vue
@@ -1,16 +1,15 @@
 <template>
   <nav class="uk-navbar-container uk-background-primary uk-navbar-transparent uk-light" uk-navbar>
 
-    <router-link class="uk-navbar-item uk-logo"   to="/">Give</router-link>
+    <router-link class="uk-navbar-item uk-logo uk-margin-left"   to="/">Give</router-link>
 
-    <div class="uk-navbar-right">
+    <div class="uk-navbar-right uk-margin-right">
 
         <ul class="uk-navbar-nav">
-            <router-link active-class="uk-active" tag="li" to="/"><a>About</a></router-link>
+            <router-link tag="li" to="/"><a>About</a></router-link>
             <router-link active-class="uk-active" tag="li" to="/channels"><a>Channels</a></router-link>
-            <router-link active-class="uk-active" tag="li" to="/dashboard"><a>Dashboard</a></router-link>
-            <router-link active-class="uk-active" tag="li" to="/account/login"><a>Login</a></router-link>
-            <router-link active-class="uk-active" tag="li" to="/account/signup"><a>Sign up</a></router-link>
+            <router-link v-if="user" active-class="uk-active" tag="li" to="/dashboard"><a>Dashboard</a></router-link>
+            <router-link v-if="!user" active-class="uk-active" tag="li" to="/account/login"><a><span class="uk-logo uk-icon" uk-icon="icon: user"></span></a></router-link>
         </ul>
 
     </div>
@@ -22,6 +21,9 @@
 export default{
   // components: {
   // },
+  props: [
+    'user'
+  ],
   data() {
     return {
     };
