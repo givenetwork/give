f5cac18f7de8992019a6691ca576df2e6691beac
Klem Novak (knovak@execproinc.com) <knovak@execproinc.com>
2018-02-15 00:21:24 -0800

    Refactor channel tree viewer

--- app/Dashboard/ChannelItem.vue
+++ app/Dashboard/ChannelItem.vue
@@ -1,26 +1,43 @@
 <template>
-  <div class="channel-item">
-    <div class="uk-margin-bottom" v-bind:class="{'uk-card uk-card-default' : level>1 }" v-bind:style="{ width: itemWidth + '%' }">
-        <div class="uk-flex">
+  <div>
+    <div class="channel-item uk-flex uk-flex-wrap-stretch uk-margin-small-bottom uk-background-muted">
+      <div class="uk-flex-auto">
+        <div class="uk-card uk-card-default channel-item--card" v-bind:style="{ width: itemWidth + '%' }">
+          <div class="uk-flex">
             <div class="uk-width-expand uk-padding-small">
-                <h3 class="uk-card-title uk-text-left uk-margin-remove-bottom">
-                  <b>{{node.name}}</b>
-                </h3>
+                <div v-if="!edit">
+                  <h3 class="uk-card-title uk-text-left uk-margin-remove-bottom uk-display-inline" >
+                    <a class="uk-button uk-button-default uk-button-round" v-if="parent.canEdit" @click="toggleEditItem"><span class="uk-icon" uk-icon="icon: pencil"></span></a>
+                    <a class="uk-button uk-button-round" v-else><span class="uk-icon" uk-icon="icon: lock"></span></a>
+                    <b>{{node.name}}</b>
+                  </h3>
+                </div>
+                <div v-else>
+                  <h3 class="uk-card-title uk-text-left uk-margin-remove-bottom uk-display-inline" >
+                    <form @submit="saveItem" class="uk-display-inline">
+                      <button class="uk-button uk-button-primary uk-button-round" @click="saveItem"><span class="uk-icon" uk-icon="icon: check"></span></button>
+                      <input class="uk-input uk-width-1-6" type="text" v-model="tempScore">
+                    </form>
+                    <span>{{ node.name }}</span>
+                  </h3>
+                </div>
                 <!-- <p class="uk-text-meta uk-margin-remove-top"><time datetime="2016-04-01T19:00">April 01, 2016</time></p> -->
             </div>
-            <div class="uk-width-auto uk-text-right uk-padding-small" v-if="level>1">
+            <div class="uk-width-auto uk-text-right uk-padding-small">
               <div class="">
                 <span class="uk-hidden@m uk-margin-small-right">{{ itemWeight }}%</span>
-                <a href="#" class="uk-button uk-button-default uk-button-round"><span class="uk-icon" uk-icon="icon: pencil"></span></a>
-                <a href="#" class="uk-button uk-button-primary uk-button-round" v-if="node.children" @click="makeTop(node)"><span class="uk-icon" uk-icon="icon: chevron-right"></span></a>
               </div>
             </div>
             <div class="uk-visible@m uk-button uk-width-auto uk-flex uk-flex-middle channel-item--expand" v-if="!!itemWeight" v-bind:class="{ 'uk-button-primary': node.type=='channel', 'uk-button-secondary': node.type=='user'}">
-                {{ itemWeight }}%
+                ({{ itemWeight }}%)
             </div>
+          </div>
         </div>
+      </div>
+      <div class="uk-flex-none uk-padding-small">
+        <a class="uk-button uk-button-default uk-button-round" v-if="node.children" @click="makeRootCall"><span class="uk-icon" uk-icon="icon: chevron-right"></span></a>
+      </div>
     </div>
-    <ChannelItem :node="child" :parentScoreTotal="totalScore" :parentScoreMax="maxScore" v-for="child in sortedChildren" :level="level+1" v-if="level<2"/>
   </div>
 </template>
 
@@ -30,19 +47,15 @@ var _ = require('lodash');
 
 export default {
   name: 'ChannelItem',
-  store: [
-    'channelTree'
-  ],
   props: {
+    parent: Object,
     node: Object,
-    parentScoreTotal: Number,
-    parentScoreMax: Number,
-    level: Number,
-    resetTree: Function
+    makeRoot: Function,
   },
   data: function () {
     return {
-      open: false
+      edit: false,
+      tempScore: this.node.score
     }
   },
   computed: {
@@ -51,46 +64,29 @@ export default {
         this.node.children.length
     },
     itemWeight() {
-      if(!this.parentScoreTotal) return false
-      var width = (100 * this.node.score / this.parentScoreTotal).toFixed(0)
+      if(!this.node.parentTotalScore) return fals
+      var width = (100 * this.node.score / this.node.parentTotalScore).toFixed(0)
       return width ? width : 100
     },
     itemWidth() {
-      var width = (15 + (85 * this.node.score / this.parentScoreMax)).toFixed(2)
+      var width = (15 + (78 * this.node.score / this.node.parentMaxScore)).toFixed(2)
       return width
-    },
-    totalScore() {
-      return _.sumBy(this.node.children, 'score' )
-    },
-    maxScore() {
-      return _.maxBy(this.node.children, 'score').score
-    },
-    sortedChildren() {
-      return _.orderBy(this.node.children, 'score', 'desc')
     }
   },
   methods: {
-    toggle: function () {
-      if (this.isFolder) {
-        this.open = !this.open
-      }
+    toggleEditItem() {
+      this.edit = !this.edit
     },
-    makeTop: function(data) {
-      this.channelTree.backup.push(this.channelTree.data)
-      this.channelTree.data = data
+    saveItem() {
+      this.node.score = this.tempScore
+      this.edit = false
+
+      // TODO: Save item weight here
+      console.log("Item updated", this.node)
+    },
+    makeRootCall() {
+      this.makeRoot(this.node)
     }
-    // changeType: function () {
-    //   if (!this.isFolder) {
-    //     Vue.set(this.node, 'children', [])
-    //     this.addChild()
-    //     this.open = true
-    //   }
-    // },
-    // addChild: function () {
-    //   this.node.children.push({
-    //     name: 'new stuff'
-    //   })
-    // }
   }
 }
 </script>
--- app/Dashboard/Dashboard.vue
+++ app/Dashboard/Dashboard.vue
@@ -2,43 +2,22 @@
   <div>
     <div class="uk-container uk-padding">
       <h1>Dashboard</h1>
-      <!-- {{privateChannelCache}} -->
-      <!-- <div uk-grid> -->
-        <!-- <div class="uk-width-1-3@m">
-          <div class="uk-alert uk-alert-default">
-            <p>Private channels: 3</p>
-
-            <span v-for="pc in privateChannels"  v-if="privateChannels">
-              {{ pc.name }}
-            </span>
-          </div>
-        </div> -->
-        <!-- <div class="uk-width-2-3@m"> -->
-
-        <!-- </div> -->
-      <!-- </div> -->
     </div>
-    <a v-if="this.channelTree.backup.length"  @click="restoreToItem" class="uk-button uk-button-secondary uk-width-1-1 button-tree-up">LEVEL UP <span class="uk-icon" uk-icon="icon: chevron-up"></span></a>
-    <div class="uk-background-muted uk-padding">
-      <ul class="uk-breadcrumb" v-if="this.channelTree.backup.length">
-          <li v-for="(item, key) in this.channelTree.backup">
-            <a href="#" @click="restoreToItem(key)">{{ item.name }}</a>
-          </li>
-          <li><span>{{ channelTree.data.name }}</span></li>
-      </ul>
-      <ChannelItem :node="channelTree.data" :level="1" />
-    </div>
+
+    <ChannelTree :node="channelTree" />
+
   </div>
 </template>
 
 <script>
 import Util from '../Util'
-import ChannelItem from './ChannelItem'
+import ChannelTree from './ChannelTree'
 var _ = require('lodash')
 
 // demo data
 var data = {
   name: 'a483n4c (Default)',
+  canEdit: true,
   children: [
     { name: '@mikefair', score: 2, type: 'user' },
     { name: '@klemen', score: 5, type: 'user' },
@@ -51,6 +30,7 @@ var data = {
           name: '#greenpeace-east',
           score: 100,
           type: 'channel',
+          canEdit: true,
           children: [
             { name: '@waf3901', score: 1, type: 'user' },
             { name: '@boona4900003', score: 3, type: 'user' }
@@ -62,6 +42,7 @@ var data = {
           name: '#gp-west',
           score: 250,
           type: 'channel',
+          canEdit: true,
           children: [
             { name: '@nonrra', score: 9, type: 'user'},
             { name: '@good4all', score: 5, type: 'user' }
@@ -79,10 +60,10 @@ export default {
     'channelTree'
   ],
   beforeMount() {
-    this.channelTree.data = data
+    this.channelTree = data
   },
   mounted() {
-    console.log("Dashboard mounted")
+    // console.log("Dashboard mounted")
 
     Util.user.get('channels').map().on(this.addPrivateChannel)
 
@@ -102,7 +83,7 @@ export default {
     })
   },
   components: {
-    ChannelItem
+    ChannelTree
   },
   data() {
     return {
@@ -110,16 +91,6 @@ export default {
     }
   },
   methods: {
-    resetTree() {
-      this.channelTree.data = this.channelTree.backup[0]
-      this.channelTree.backup = []
-    },
-    restoreToItem(index) {
-      if(index) {
-        this.channelTree.backup.splice(index, -1)
-      }
-      this.channelTree.data = this.channelTree.backup.pop()
-    },
     addPrivateChannel: function(data, key) {
       console.log("Private channel!", data)
       if(key && data.title) {
@@ -134,5 +105,9 @@ export default {
       return privateChannels
     }
   }
+}
+
+if (module.hot) {
+  module.hot.accept();
 }
 </script>
